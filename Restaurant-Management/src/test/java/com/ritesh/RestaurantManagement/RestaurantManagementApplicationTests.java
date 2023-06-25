b2ef5bbd297bfb6d339f08f341aeec25c7af22b4
Ritesh Kr <ritesh123kumar081@gmail.com>
2023-06-25 10:43:40 +0530

    first Commit

---
+++ Restaurant-Management/src/test/java/com/ritesh/RestaurantManagement/RestaurantManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ritesh.RestaurantManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
